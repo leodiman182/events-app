1a8b171a7aab315ee4a9b487a56882f434533240
Leonardo <leonardo.diman@gmail.com>
2023-03-30 17:08:29 -0300

    Events page and events by city page layout + responsible + changed background color

--- src/pages/events/[byCity]/index.js
+++ src/pages/events/[byCity]/index.js
@@ -4,19 +4,15 @@ const EventsByCity = () => {
   return (
     <>
       <header className={ styles.header }>
-        <h2>Events</h2>
+        <h2>Events in London</h2>
       </header>
-
       <main className={ styles.main }>
-        <section>
-          <h3>Events In London</h3>
-          <ul>
-            <a href="/events/london/event1">Event 1</a>
-            <a href="/events/london/event2">Event 2</a>
-            <a href="/events/london/event3">Event 3</a>
-            <a href="/events/london/event4">Event 4</a>
-          </ul>
-        </section>
+        <ul>
+          <a href="/events/london/event1">Event 1</a>
+          <a href="/events/london/event2">Event 2</a>
+          <a href="/events/london/event3">Event 3</a>
+          <a href="/events/london/event4">Event 4</a>
+        </ul>
       </main>
       
     
