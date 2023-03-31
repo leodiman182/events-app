5df21bf70f559002b0ddcee9c813a0343a6bb6ec
Leonardo <leonardo.diman@gmail.com>
2023-03-31 10:45:37 -0300

    neon effect added

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -10,7 +10,7 @@ const EventsPage = ({ data }) => {
       <main className={ styles.main }>
         {
           data.map(ev => (
-            <a href={`/events/${ ev.id }`}>
+            <a key={ ev.id } href={`/events/${ ev.id }`}>
               <h2>{ ev.title }</h2>
               <Image width={999} height={999} src={ ev.image } alt={ ev.title } />
             </a>
